fd59652b56c7d5ae4be5150c72e75bc430072ead
lonerae <78692050+lonerae@users.noreply.github.com>
2023-02-07 11:46:02 +0200

    cleanup & documentation

--- treeRepresentation.c
+++ treeRepresentation.c
@@ -3,26 +3,130 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define MAX_LENGTH 50
+#define MAX_EXPRESSION_LENGTH 50
 
-void clean(char *e, char *ans) {
-	int i = 0;
-	int j = 0;
-	while (i < MAX_LENGTH) {
-		if (!isspace(*(e+i))) {
-			if (*(e+i) == '(' || *(e+i) == '{' || *(e+i) == '[') {
-				*(ans+j) = '(';
-			} else if (*(e+i) == ')' || *(e+i) == '}' || *(e+i) == ']') {
-				*(ans+j) = ')';
+/**
+* Returns a numeric expression where all white spaces are trimmed
+* and all parentheses, brackets and braces are replaced by parentheses.
+*
+* Parameters
+*	expression - pointer to the initial string
+	result - pointer to the result
+*/
+void cleanExpression(char *expression, char *result) {
+	int expressionIndex = 0;
+	int resultIndex = 0;
+
+	while (expressionIndex < MAX_EXPRESSION_LENGTH) {
+		if (!isspace(*(expression+expressionIndex))) {
+			if (*(expression+expressionIndex) == '(' || *(expression+expressionIndex) == '{' || *(expression+expressionIndex) == '[') {
+				*(result+resultIndex) = '(';
+			} else if (*(expression+expressionIndex) == ')' || *(expression+expressionIndex) == '}' || *(expression+expressionIndex) == ']') {
+				*(result+resultIndex) = ')';
 			} else  {
-				*(ans+j) = *(e+i);
+				*(result+resultIndex) = *(expression+expressionIndex);
 			}
-			j++;
+			resultIndex++;
 		}		
-		i++;
+		expressionIndex++;
 	}
 }
 
+/**
+* Removes the outer parentheses of an expression.
+* 
+* Parameters
+*	expression - pointer to the given expression
+*
+* WARNING: TREATS THE EXPRESSION (STRING i.e. character array) AS A MUTABLE OBJECT
+*/
+void simplify(char *expression) {
+	int length = strlen(expression);
+	if (*(expression+0) == '(' && *(expression+length-1) == ')') {
+		for (int expressionIndex=1; expressionIndex<length-1;expressionIndex++) {
+			*(expression+expressionIndex-1) = *(expression+expressionIndex);
+		}		
+		*(expression+length-2) = '\0';
+	}
+}
+
+/**
+* Returns the left operand of a numeric expression and
+* informs whether it was an expression or a number.
+* e.g.
+*	if expression pointed to [1-(2*5)]+3-[(2-4)/3],
+*	left would point to 1-(2*5) and leftFlag to 0
+*
+* Parameters
+*	expression - pointer to the expression
+*	left - pointer to the left operand
+*	leftFlag - pointer to 1 if left is just a number, otherwise to 0
+*/
+void findLeft(char *expression, char *left, int *leftFlag) {
+	int expressionIndex = 0;
+	int leftIndex = 0;
+	int length = strlen(expression);
+	 
+	if (*(expression+expressionIndex) == '(') {
+		int nestedParentheses = 0;
+		expressionIndex++;
+		while (*(expression+expressionIndex) != ')' || nestedParentheses != 0) {
+			*(left+leftIndex) = *(expression+expressionIndex);
+			if (*(expression+expressionIndex) == '(') {
+				nestedParentheses++;
+			} else if (*(expression+expressionIndex) == ')') {
+				nestedParentheses--;
+			}	
+			leftIndex++;
+			expressionIndex++;
+		}
+		*(left+leftIndex) = '\0';
+		*leftFlag = 0;
+		expressionIndex++;
+	} else {
+		while (isdigit(*(expression+expressionIndex))) {
+			*(left+leftIndex) = *(expression+expressionIndex);
+			leftIndex++;
+			expressionIndex++;
+		}
+		*(left+leftIndex) = '\0';
+	}
+}
+
+/**
+* Returns the right operand of a numeric expression and
+* informs whether it was an expression or a number.
+* e.g.
+*	if expression pointed to [1-(2*5)]+3-[(2-4)/3],
+*	right would point to 3-[(2-4)/3] and rightFlag to 0
+*
+* Parameters
+*	expression - pointer to the expression
+*	right - pointer to the right operand
+*	startIndex - index after which we should evaluate the right operand (depending on left operand length)
+*	rightFlag - pointer to 1 if right is just a number, otherwise to 0
+*/
+void findRight(char *expression, char *right, int startIndex, int *rightFlag) {
+	int rightIndex = 0;
+	int expressionIndex = startIndex;
+	
+	while (*(expression+expressionIndex) != '\0') {
+		if (!isdigit(*(expression+expressionIndex))) {
+			*rightFlag = 0;
+		}
+		*(right+rightIndex) = *(expression+expressionIndex);
+		expressionIndex++;
+		rightIndex++;
+	}
+	*(right+rightIndex) = '\0';
+	simplify(right);
+}
+
+/**
+* Node structure for the generated tree. Each node
+* either has an operator character (inner node) or 
+* a value integer (leaf node).
+*/
 struct treeNode {
 	char operator;
 	int value;
@@ -34,113 +138,65 @@ struct treeNode {
 
 struct treeNode *root;
 
-void findLeft(char *e, char *left, int *leftFlag) {
-	int i = 0;
-	int length = strlen(e); 
-	if (*(e+i) == '(') {
-		int count = 0;
-		int j = 0;
-		i++;
-		while (*(e+i) != ')' || count != 0) {
-			*(left+j) = *(e+i);
-			if (*(e+i) == '(') {
-				count++;
-			} else if (*(e+i) == ')') {
-				count--;
-			}	
-			j++;
-			i++;
-		}
-		*(left+j) = '\0';
-		i++;
-	} else {
-		int j = 0;
-		while (isdigit(*(e+i))) {
-			*(left+j) = *(e+i);
-			j++;
-			i++;
-		}
-		*(left+j) = '\0';
-		*leftFlag = 1;
-	}
-	//printf("(SUBTREE) LEFT: %s\n",left);
-}
+/**
+* Recursively creates a tree representation of a given expression
+*
+* Parameters
+*	expression - pointer to the expression
+*/
+struct treeNode *transform(char *expression) {
+	int length = strlen(expression); 
 
-void simplify(char *right, int *rightFlag) {
-	int l = strlen(right);
-	if (*right == '(' && *(right+l-1) == ')') {
-		for (int i=1; i<l-1;i++) {
-			*(right+i-1) = *(right+i);
-		}		
-		*(right+l-2) = '\0';
-	}
-}
-
-void findRight(char *e, char *right, int startIndex, int *rightFlag) {
-	int z = 0;
-	int i = startIndex;
-	while (*(e+i) != '\0') {
-		if (!isdigit(*(e+i))) {
-			*rightFlag = 0;
-		}
-		*(right+z) = *(e+i);
-		i++;
-		z++;
-	}
-	*(right+z) = '\0';
-	simplify(right,rightFlag);
-	//printf("(SUBTREE) RIGHT: %s\n",right);
-}
-
-struct treeNode *transform(char *e) {
-	int length = strlen(e); 
-	char left[length];
-	char right[length];
+	char left[length]; 
 	int *leftFlag = (int *) malloc(sizeof(int));
+	*leftFlag = 1;
+
+	char right[length];
 	int *rightFlag = (int *) malloc(sizeof(int));
-	*leftFlag = 0; *rightFlag = 1;
-	findLeft(e,left,leftFlag);
+	*rightFlag = 1;
 
 	struct treeNode *parent = (struct treeNode*) malloc(sizeof(struct treeNode));	
-	struct treeNode *leftChild = (struct treeNode*) malloc(sizeof(struct treeNode));
-	struct treeNode *rightChild = (struct treeNode*) malloc(sizeof(struct treeNode));
+	// makes certain that the root is set on the first and only the first call of the function
 	if (root == NULL) {
 		root = (struct treeNode*) malloc(sizeof(struct treeNode));
 		root = parent;
 	}
 	
+	struct treeNode *leftChild = (struct treeNode*) malloc(sizeof(struct treeNode));
+	struct treeNode *rightChild = (struct treeNode*) malloc(sizeof(struct treeNode));
+	
+	findLeft(expression,left,leftFlag);
 	if (*leftFlag) {
-		parent->operator = *(e+strlen(left));
-		leftChild->value = atoi(left);
-		//printf("(LEAF) Left: %d\n",leftChild->value);
-		
-		findRight(e,right,strlen(left)+1,rightFlag);
+		parent->operator = *(expression+strlen(left));
+		leftChild->value = atoi(left);	
+
+		findRight(expression,right,strlen(left)+1,rightFlag);
 	} else {
-		parent->operator = *(e+strlen(left)+2);
+		parent->operator = *(expression+strlen(left)+2);
 		leftChild = transform(left);
 		
-		findRight(e,right,strlen(left)+3,rightFlag);
+		findRight(expression,right,strlen(left)+3,rightFlag);
 	}
-	
-	
-	
 	parent->left = leftChild;	
 	leftChild->above = parent;
 	
 	if (*rightFlag) {
 		rightChild->value = atoi(right);
-		//printf("(LEAF) Right: %d\n",rightChild->value);
 	} else {
 		rightChild = transform(right);
 	}
 	parent->right = rightChild;
 	rightChild->above = parent;
 	
-	
-	
 	return parent;
 }
 
+/**
+* Prints the post-order traversal of a subtree
+*
+* Parameters
+* 	node - the root node of the subtree
+*/
 void traverseTree(struct treeNode* node) {
 	if (node->left != NULL) {
 		traverseTree(node->left);
@@ -160,16 +216,18 @@ void traverseTree(struct treeNode* node) {
 }
 
 int main() {
-	char e[MAX_LENGTH];
-	char ans[MAX_LENGTH];
+	char expression[MAX_EXPRESSION_LENGTH];
 	printf("Write an arithmetic expression: \n");
-	fgets(e,MAX_LENGTH,stdin);
+	fgets(expression,MAX_EXPRESSION_LENGTH,stdin);
 	
-	clean(e,ans);
-	//printf("New form: %s\n",ans);
-	transform(ans);
+	char result[MAX_EXPRESSION_LENGTH];	
+	cleanExpression(expression,result);
+	printf("Clean form: %s\n",result);
+	
+	transform(result);
 	traverseTree(root);
 	printf("\n");
+	printf("%s\n",evaluatedExpression);
 	
 	return 0;
 }
