cf89f475f5fb810c03a0a97a7c22bd2691bd3d08
lonerae <78692050+lonerae@users.noreply.github.com>
2023-02-07 23:31:46 +0200

    added logic for list of lists (subject -> student group)

--- ex3.c
+++ ex3.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <string.h>
+#include <stdlib.h>
 
 struct subjectNode {
 	char subject[50];
@@ -10,32 +11,6 @@ struct subjectNode {
 
 struct subjectNode *head;
 
-void addSubject(struct student *studentPtr) {
-	if (head == NULL) {
-		struct subjectNode *newSubjectPtr;
-		strcpy(newSubjectPtr->subject,studentPtr->subject);
-		newSubjectPtr->ptr = studentPtr;
-
-		newSubjectPtr->next = NULL;
-		head = newSubjectPtr;
-	} 
-
-}
-
-int subjectExists(char *subject[50]) {
-	struct subjectNode *temp;
-	temp = head;
-
-	if (temp == NULL) {
-		return 1;
-	} else {
-		if (strcmp(temp->subject,subject) == 0) {
-			return 1;
-		}
-	}
-	return 0;
-}
-
 struct student {
 	char id[9];
 	char firstName[20];
@@ -43,9 +18,46 @@ struct student {
 	char city[20];
 	int age;
 	char subject[50];
+	
+	struct student *next;
 };
 
-struct student studentArray[3];
+struct student *studentArray[3];
+
+struct subjectNode *subjectExists(char subject[50]) {
+	struct subjectNode *temp = (struct subjectNode*) malloc(sizeof(struct subjectNode));
+	temp = head;
+
+	if (temp == NULL) {
+		return NULL;
+	} else {
+		if (strcmp(temp->subject,subject) == 0) {
+			return temp;
+		}
+	}
+	return NULL;
+}
+
+void addSubject(struct student *studentPtr) {
+	/* NEEDLESSLY COMPLICATED BUT WORKS -> WILL OPTIMISE*/
+	struct subjectNode *p = (struct subjectNode*) malloc(sizeof(struct subjectNode));
+	p = subjectExists(studentPtr->subject);
+	if (p == NULL) {
+		struct subjectNode *newSubjectPtr= (struct subjectNode*) malloc(sizeof(struct subjectNode));
+		strcpy(newSubjectPtr->subject,studentPtr->subject);
+		newSubjectPtr->ptr = studentPtr;
+
+		newSubjectPtr->next = head;
+		head = newSubjectPtr;
+	} else {
+		struct student *temp = (struct student*) malloc(sizeof(struct student));
+		temp = p->ptr;
+		while (temp->next != NULL) {
+			temp = temp->next;
+		}
+		temp->next = studentPtr;
+	}
+}
 
 void readStudent(struct student *s1) {
 	printf("===Student with id %s===\n",s1->id);
@@ -56,42 +68,47 @@ void readStudent(struct student *s1) {
 
 void readAllStudents() {
 	for (int i=0; i<3; i++) {
-		readStudent(studentArray + i);
+		readStudent(*(studentArray + i));
 	}
 }
 
 int main() {
-	struct student s1;	
-	strcpy(s1.id,"mppl2204");
-	strcpy(s1.firstName,"Minas");
-	strcpy(s1.lastName,"Alvertis");
-	strcpy(s1.city,"Glifada");
-	s1.age = 26;
-	strcpy(s1.subject,"Logic Programming");
+	struct student *s1 = (struct student*) malloc(sizeof(struct student));	
+	strcpy(s1->id,"mppl2204");
+	strcpy(s1->firstName,"Minas");
+	strcpy(s1->lastName,"Alvertis");
+	strcpy(s1->city,"Glifada");
+	s1->age = 26;
+	strcpy(s1->subject,"Logic Programming");
 	addSubject(s1);
 
-	struct student s2;	
-	strcpy(s2.id,"mppl2205");
-	strcpy(s2.firstName,"John");
-	strcpy(s2.lastName,"Doe");
-	strcpy(s2.city,"Peristeri");
-	s2.age = 20;
-	strcpy(s2.subject,"Logic Programming");
+	struct student *s2 = (struct student*) malloc(sizeof(struct student));	
+	strcpy(s2->id,"mppl2205");
+	strcpy(s2->firstName,"John");
+	strcpy(s2->lastName,"Doe");
+	strcpy(s2->city,"Peristeri");
+	s2->age = 20;
+	strcpy(s2->subject,"Logic Programming");
 	addSubject(s2);
 
-	struct student s3;	
-	strcpy(s3.id,"mppl2206");
-	strcpy(s3.firstName,"Mary");
-	strcpy(s3.lastName,"Christmas");
-	strcpy(s3.city,"Koropi");
-	s3.age = 24;
-	strcpy(s3.subject,"Discrete Maths");
+	struct student *s3 = (struct student*) malloc(sizeof(struct student));	
+	strcpy(s3->id,"mppl2206");
+	strcpy(s3->firstName,"Mary");
+	strcpy(s3->lastName,"Christmas");
+	strcpy(s3->city,"Koropi");
+	s3->age = 24;
+	strcpy(s3->subject,"Discrete Maths");
 	addSubject(s3);
 
 	studentArray[0] = s1;
 	studentArray[1] = s2;
 	studentArray[2] = s3;
 	
-	readAllStudents();
+	//readAllStudents();
+	
+	/* JUST FOR TESTING IGNORE */
+	printf("%s :\n",head->next->subject);
+	readStudent(head->next->ptr);
+	readStudent(head->next->ptr->next);
 	return 0;
 }
