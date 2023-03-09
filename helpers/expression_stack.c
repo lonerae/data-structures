b0c0ebb5c6236af4804f8c333c9bccf64f913b04
ekaltsuni <112812015+ekaltsuni@users.noreply.github.com>
2023-03-09 15:43:49 +0200

    Update expression_stack.c

--- helpers/expression_stack.c
+++ helpers/expression_stack.c
@@ -37,6 +37,12 @@ int pop () {
     	}
 }
 
+/**
+* Loops through each row to identify when string is digit with function isdigit
+* and repeats push or pop functions based on that
+* if a string is a digit, it converts the string to digit with atoi and pushes the digit to the stack
+* if not, it pops the last two digits and calculates their result. 
+*/
 void evaluateResult(char evaluatedExpression[MAX_EXPRESSION_LENGTH][MAX_EXPRESSION_LENGTH]) {
 	int digit, j;
 	int temp1, temp2;
